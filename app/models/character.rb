11073a18c7b3756a017795ed1e1ff94617ecc170
Brianna Forster <brianna.forster@gmail.com>
2017-08-08 22:47:59 -0700

    Create table for RACE

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ActiveRecord::Base
   belongs_to :user
+  belongs_to :race
 
   validates :name, :character_class, :race, :level, :strength, :dexterity, :intelligence,
       :wisdom, :charisma, :user_id, { presence: true }
