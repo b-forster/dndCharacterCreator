f8c231dc5bfca4f246d8fe140e9a2525a09a9d74
Brianna Forster <brianna.forster@gmail.com>
2017-08-24 22:38:39 -0700

    Add validations on user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :characters
 
   validates :email, :username, presence: true, uniqueness: true
+  validate :is_valid_password?
   validates_format_of :email, :with => /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
 
   def password
@@ -13,7 +14,16 @@ class User < ActiveRecord::Base
   end
 
   def password=(new_password)
+    @plain_text_password = new_password
     @password = Password.create(new_password)
     self.password_hash = @password
+  end
+
+  def is_valid_password?
+    if @plain_text_password.length == 0
+      errors.add(:password, "can't be blank")
+    elsif @plain_text_password.length < 6
+      errors.add(:password, "must be 6 or more characters")
+    end
   end
 end
